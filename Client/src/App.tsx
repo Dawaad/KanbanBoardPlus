446ac45f8f64328df4f82e5c2f0ebfcf5272e391
Dawaad <jntucker02@gmail.com>
2023-08-01 18:25:08 +1000

    Set up base Vite + Tailwind configuration

---
+++ Client/src/App.tsx
@@ -0,0 +1,8 @@
+import DashboardBoard from "./Components/Dashboard/Board/DashboardBoard";
+import DashboardHeader from "./Components/Dashboard/DashboardHeader";
+
+function App() {
+  return <main className=" "></main>;
+}
+
+export default App;

1b76220aef89962c00f0f1c3a889ca7cec04cfe9
Dawaad <jntucker02@gmail.com>
2023-08-09 13:01:05 +1000

    Update Layout Settings

--- Client/src/Layouts/Layout.tsx
+++ Client/src/Layouts/Layout.tsx
@@ -1,9 +1,117 @@
 import React from "react";
 import { Link, Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { SunIcon, MoonIcon } from "@heroicons/react/24/solid";
 function Layout({ children }: { children: React.ReactNode }) {
+  const html = document.querySelector("html");
+  const userTheme = localStorage.getItem("theme");
+  const systemTheme = window.matchMedia("(prefers-color-scheme: dark)").matches
+    ? "dark"
+    : "light";
+
+  const themeSwitch = () => {
+    if (html) {
+      if (html.classList.contains("dark")) {
+        html.classList.remove("dark");
+        localStorage.setItem("theme", "light");
+      } else {
+        html.classList.add("dark");
+        localStorage.setItem("theme", "dark");
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (html) {
+      if (userTheme === "dark" || systemTheme === "dark") {
+        html.classList.add("dark");
+        localStorage.setItem("theme", "dark");
+      } else {
+        html.classList.remove("dark");
+        localStorage.setItem("theme", "light");
+      }
+    }
+  }, []);
+
   return (
-    <div className="">
-      <div className="">{children ? children : <Outlet></Outlet>}</div>
+    <div className="overflow-y-hidden h-screen">
+      {/* Your common layout elements */}
+      <header className="h-[3.75rem] bg-zinc-200/40 dark:bg-zinc-800/50 border-b border-b-zinc-600/60  dark:border-b-zinc-400/60">
+        <div className="flex flex-col md:flex-row items-center px-5 py-3 ">
+          {/*Title*/}
+          <Link
+            to={"/"}
+            id="nav-logo"
+            className=" h-fit text-3xl font-extrabold flex group cursor-pointer text-zinc-800/80 dark:text-zinc-200/80 "
+          >
+            <div className="flex">
+              <p>K</p>
+              {["a", "n", "b", "a", "n"].map((letter, index) => {
+                return (
+                  <p
+                    key={letter}
+                    className={`scale-0  group-hover:scale-100 origin-bottom`}
+                    style={{ transition: `${200 + 200 * index}ms` }}
+                  >
+                    {letter}
+                  </p>
+                );
+              })}
+              <div className="flex transition-all duration-1000 group-hover:duration-700 -translate-x-full group-hover:-translate-x-0 group-hover:mx-2">
+                <p>B</p>
+                {["o", "a", "r", "d"].map((letter, index) => {
+                  return (
+                    <p
+                      key={letter}
+                      className={`scale-0  group-hover:scale-100 origin-bottom `}
+                      style={{ transition: `${200 + 200 * index}ms ` }}
+                    >
+                      {letter}
+                    </p>
+                  );
+                })}
+              </div>
+              <div className="flex transition-all -translate-x-[9.8rem] group-hover:-translate-x-0 duration-1000 group-hover:duration-700 ">
+                <p className="block group-hover:hidden text-xl">+</p>
+                <div className="flex text-3xl">
+                  {["P", "l", "u", "s"].map((letter, index) => {
+                    return (
+                      <p
+                        key={letter}
+                        className={`scale-0  group-hover:scale-100 origin-bottom `}
+                        style={{ transition: `${200 + 200 * index}ms ` }}
+                      >
+                        {letter}
+                      </p>
+                    );
+                  })}
+                </div>
+              </div>
+            </div>
+          </Link>
+          {/*Search Bar*/}
+          <div className="flex items-center space-x-10  flex-1 justify-end w-full">
+            <Link
+            to={'/login'}
+            className="text-xl font-semibold text-zinc-800/80 dark:text-zinc-300/80 hover:text-blue-800 hover:dark:text-blue-400"
+            >
+              Login
+            </Link>
+            <button
+              className="hover:scale-110 transition-all text-zinc-800/90 dark:text-zinc-300 hover:text-slate-500"
+              onClick={() => {
+                themeSwitch();
+              }}
+            >
+              <SunIcon className="w-6 h-6 hidden dark:block" />
+              <MoonIcon className="w-6 h-6 block dark:hidden" />
+            </button>
+          </div>
+        </div>
+      </header>
+
+      <div>{children ? children : <Outlet></Outlet>}</div>
+      {/* Other layout elements */}
     </div>
   );
 }

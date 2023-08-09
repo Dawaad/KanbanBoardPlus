537771b51d82001602343694ec35de16741c148f
Dawaad <jntucker02@gmail.com>
2023-08-09 13:00:51 +1000

    Initialise Dashboard Layout

--- Client/src/pages/my/Dashboard.tsx
+++ Client/src/pages/my/Dashboard.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
+import Sideboard from "../../Components/Dash/Sideboard";
 
 function Dashboard() {
   return (
-    <div className=''>
-      Dashboard
+    <div className="grid  grid-cols-3">
+      <div className="col-span-1">
+        <Sideboard />
+      </div>
+      <div className="col-span-2"> yes </div>
     </div>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;

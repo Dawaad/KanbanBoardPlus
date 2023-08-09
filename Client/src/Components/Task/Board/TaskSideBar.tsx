37a34bfe3514e2c5ea7d7a3e7d8408e018c38e84
Dawaad <jntucker02@gmail.com>
2023-08-09 13:00:16 +1000

    Update Taskboard Positioning

--- Client/src/Components/Task/Board/TaskSideBar.tsx
+++ Client/src/Components/Task/Board/TaskSideBar.tsx
@@ -23,13 +23,13 @@ const users = [
 ];
 function TaskSideBar() {
   return (
-    <section className="w-[2.5rem] hover:w-[20rem] bg-zinc-200/40 dark:bg-zinc-800/50  transition-all  ">
+    <aside className="w-[3rem] hover:w-[20rem] bg-zinc-200/40 dark:bg-zinc-800/50  transition-all  ">
       <section
         className="py-4 border-b border-b-zinc-600/60 dark:border-b-zinc-400/60"
         id="section-social"
       >
         <div className="flex items-center overflow-hidden pb-2">
-          <div className="ml-1 mr-2 ">
+          <div className="ml-2 mr-3 ">
             {/*Sample Avatar, Replace Dynamically with Users*/}
 
             {/*This should be the avatar of the current user */}
@@ -55,14 +55,14 @@ function TaskSideBar() {
               );
             })}
           </div>
-          <div className="text-zinc-700 dark:text-zinc-300 text-sm ml-1  mt-3">
+          <div className="text-zinc-700 dark:text-zinc-300 text-smml-mr-33">
             {" "}
             +{`${users.slice(3).length}`}{" "}
           </div>
         </div>
 
-        <div className="flex items-center overflow-hidden hover:bg-zinc-400/30 hover:dark:bg-zinc-500/30">
-          <div className="ml-1 mr-2 my-2">
+        <div className="flex items-center overflow-hidden hover:bg-zinc-400/30 hover:dark:bg-zinc-500/30 cursor-pointer">
+          <div className="ml-2 mr-3 my-2">
             <ShareIcon className="w-7 h-6 text-zinc-800/90 dark:text-zinc-300" />
           </div>
 
@@ -76,8 +76,8 @@ function TaskSideBar() {
         id="section-history"
         className="border-b border-b-zinc-600/60 dark:border-b-zinc-400/60 space-y-2 py-4"
       >
-        <div className="flex items-center overflow-hidden hover:bg-zinc-400/30 hover:dark:bg-zinc-500/30">
-          <div className="ml-1 mr-2 my-2">
+        <div className="flex items-center overflow-hidden hover:bg-zinc-400/30 hover:dark:bg-zinc-500/30 cursor-pointer">
+          <div className="ml-2 mr-3 my-2">
             <ClockIcon className="w-7 h-6 text-zinc-800/90 dark:text-zinc-300" />
           </div>
 
@@ -85,8 +85,8 @@ function TaskSideBar() {
             View History
           </div>
         </div>
-        <div className="flex items-center overflow-hidden hover:bg-zinc-400/30 hover:dark:bg-zinc-500/30">
-          <div className="ml-1 mr-2 my-2">
+        <div className="flex items-center overflow-hidden hover:bg-zinc-400/30 hover:dark:bg-zinc-500/30 cursor-pointer">
+          <div className="ml-2 mr-3 my-2">
             <PresentationChartLineIcon className="w-7 h-6 text-zinc-800/90 dark:text-zinc-300" />
           </div>
 
@@ -94,8 +94,8 @@ function TaskSideBar() {
             Activity
           </div>
         </div>
-        <div className="flex items-center overflow-hidden hover:bg-zinc-400/30 hover:dark:bg-zinc-500/30">
-          <div className="ml-1 mr-2 my-2">
+        <div className="flex items-center overflow-hidden hover:bg-zinc-400/30 hover:dark:bg-zinc-500/30 cursor-pointer">
+          <div className="ml-2 mr-3 my-2">
             <ArchiveBoxArrowDownIcon className="w-7 h-6 text-zinc-800/90 dark:text-zinc-300" />
           </div>
 
@@ -106,8 +106,8 @@ function TaskSideBar() {
       </section>
 
       <section id="section-board" className="py-4 space-y-2">
-        <div className="flex items-center overflow-hidden hover:bg-zinc-400/30 hover:dark:bg-zinc-500/30">
-          <div className="ml-1 mr-2 my-2">
+        <div className="flex items-center overflow-hidden hover:bg-zinc-400/30 hover:dark:bg-zinc-500/30 cursor-pointer">
+          <div className="ml-2 mr-3 my-2">
             <CogIcon className="w-7 h-7 text-zinc-800/90 dark:text-zinc-300" />
           </div>
 
@@ -115,8 +115,8 @@ function TaskSideBar() {
             Settings
           </div>
         </div>
-        <div className="flex items-center overflow-hidden hover:bg-zinc-400/30 hover:dark:bg-zinc-500/30">
-          <div className="ml-1 mr-2 my-2">
+        <div className="flex items-center overflow-hidden hover:bg-zinc-400/30 hover:dark:bg-zinc-500/30 cursor-pointer">
+          <div className="ml-2 mr-3 my-2">
             <AdjustmentsHorizontalIcon className="w-7 h-7 text-zinc-800/90 dark:text-zinc-300" />
           </div>
 
@@ -125,7 +125,7 @@ function TaskSideBar() {
           </div>
         </div>
       </section>
-    </section>
+    </aside>
   );
 }
 

84dfae293890ec68e23776fc23b9e86567eab311
Dawaad <jntucker02@gmail.com>
2023-08-15 14:14:14 +1000

    Remove Dashboard Auth Check

--- Client/src/pages/my/Dashboard.tsx
+++ Client/src/pages/my/Dashboard.tsx
@@ -1,24 +1,13 @@
 import React, { useEffect, useState } from "react";
 import Sideboard from "../../Components/Dash/Sideboard";
-import { User, onAuthStateChanged, getAuth } from "firebase/auth";
+import { User, onAuthStateChanged, getAuth, Auth } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
-
+import axios from "axios";
 function Dashboard() {
-  const auth = getAuth();
   const navigate = useNavigate();
   const [user, setUser] = useState<User | null>(null);
 
-  useEffect(() => {
-    //Check User Authentication Status
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        console.log(user.displayName);
-        setUser(user);
-      } else {
-        navigate("/login");
-      }
-    });
-  }, []);
+ 
 
   return (
     <div className="grid  grid-cols-3">

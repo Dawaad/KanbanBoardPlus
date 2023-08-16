de9adc33e5f8e09d0a387503820e2e767f80cd09
Dawaad <jntucker02@gmail.com>
2023-08-16 21:42:48 +1000

    Update Dashboard Sidebar Content

--- Client/src/Components/Dash/Sideboard.tsx
+++ Client/src/Components/Dash/Sideboard.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
 
 function Sideboard() {
   return (
-    <div>
-      
+    <div className="w-[15rem] lg:w-[20rem] p-4  border-r border-r-zinc-600/60  dark:border-r-zinc-400/60">
+      <div className="flex flex-row">
+        <header className="font-bold text-lg text-zinc-800/80 dark:text-zinc-200/80">Boards</header>
+        
+      </div>
+      <section></section>
     </div>
-  )
+  );
 }
 
-export default Sideboard
+export default Sideboard;

d727963825eef2c5dc61a42eac9ca1420e595593
Dawaad <jntucker02@gmail.com>
2023-08-19 11:46:40 +1000

    Update Dashboard Design and Implement Authentication Fallbacks

--- Client/src/Components/Dash/Sideboard.tsx
+++ Client/src/Components/Dash/Sideboard.tsx
@@ -1,13 +1,24 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 function Sideboard() {
   return (
-    <div className="w-[15rem] lg:w-[20rem] p-4  border-r border-r-zinc-600/60  dark:border-r-zinc-400/60">
-      <div className="flex flex-row">
-        <header className="font-bold text-lg text-zinc-800/80 dark:text-zinc-200/80">Boards</header>
-        
+    <div className="w-[15rem] lg:w-[17rem] h-full py-4  border-r border-r-zinc-600/60  dark:border-r-zinc-400/60">
+      <div className="flex flex-col">
+        <header className="font-bold text-lg px-3 text-zinc-800/80 dark:text-zinc-200/80">
+          Boards
+        </header>
+        <section className="my-3  pb-3 border-b border-b-zinc-600/60 dark:border-b-zinc-400/60">
+          <ul className="mx-6 space-y-2 ">
+            {[1, 2, 3, 4, 5].map((index) => {
+              return (
+                <li className="text-sm font-semibold dark:text-zinc-400">
+                  Project {index}
+                </li>
+              );
+            })}
+          </ul>
+        </section>
       </div>
-      <section></section>
     </div>
   );
 }
--- Client/src/pages/my/Dashboard.tsx
+++ Client/src/pages/my/Dashboard.tsx
@@ -1,20 +1,34 @@
 import React, { useEffect, useState } from "react";
 import Sideboard from "../../Components/Dash/Sideboard";
 import { User, onAuthStateChanged, getAuth, Auth } from "firebase/auth";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { PlusCircleIcon, PlusIcon } from "@heroicons/react/24/solid";
 import axios from "axios";
 function Dashboard() {
   const navigate = useNavigate();
   const [user, setUser] = useState<User | null>(null);
 
+  const auth = getAuth();
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        navigate("/loginerror");
+      }
+    });
+  }, []);
+
   return (
-    <div className="grid  md:flex md:flex-row">
+    <div className="grid  md:flex md:flex-row h-[70rem]">
       <aside className="hidden md:block">
         <Sideboard />
       </aside>
       <main className="p-4">
-        <section id="user-boards">
+        <h1 className="hidden md:block md:text-2xl lg:text-4xl font-bold  my-4 text-zinc-800/80 dark:text-zinc-200">
+          Hello, {auth.currentUser?.displayName}
+        </h1>
+        <section id="user-boards" className="mx-4">
           <div className="flex flex-row items-center pb-4">
             <h1 className="text-lg md:text-2xl font-bold text-zinc-800/80 dark:text-zinc-200/80">
               Your boards
@@ -26,7 +40,29 @@ function Dashboard() {
           <div className="grid grid-cols-2 md:flex flex-row flex-wrap p-4">
             {[1, 2, 3, 4, 5].map((index: number) => {
               return (
-                <div className=" border m-4 md:w-[13rem] h-[8rem] bg-orange-300">{index}</div>
+                //This should be a link to the projects id which we can implement later
+                <Link to={'board/aaa'} className="m-4 md:w-[11rem] lg:w-[13rem] h-[7rem] transition-all bg-orange-400 flex justify-center items-center shadow-md shadow-zinc-800 dark:shadow-none rounded-md hover:opacity-70">
+                  <p className="text-lg font-semibold">Project {index}</p>
+                </Link>
+              );
+            })}
+          </div>
+        </section>
+        <section id="shared-boards" className="m-4">
+          <div className="flex flex-row items-center pb-4">
+            <h1 className="text-lg md:text-2xl font-bold text-zinc-800/80 dark:text-zinc-200/80">
+              Shared Boards
+            </h1>
+            <button className="flex md:hidden px-2 font-bold text-zinc-800/80 dark:text-zinc-200/80">
+              <PlusIcon className="w-6 h-6" />
+            </button>
+          </div>
+          <div className="grid grid-cols-2 md:flex flex-row flex-wrap p-4">
+            {[1, 2, 3].map((index: number) => {
+              return (
+                <Link to={"board/aaa"} className="m-4 md:w-[11rem] lg:w-[13rem] h-[7rem] transition-all bg-red-400 flex justify-center items-center shadow-md shadow-zinc-800 dark:shadow-none rounded-md hover:opacity-70">
+                  <p className="text-lg font-semibold">Project {index}</p>
+                </Link>
               );
             })}
           </div>

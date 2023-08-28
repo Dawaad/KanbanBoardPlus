f3782afc608b23a356b2cd01edcbfbd48771b536
Dawaad <jntucker02@gmail.com>
2023-08-28 17:15:40 +1000

    Create Custom Board Hook

--- Client/src/Components/Dash/Sideboard.tsx
+++ Client/src/Components/Dash/Sideboard.tsx
@@ -1,21 +1,39 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import useBoards from "@/Hooks/useBoards";
 function Sideboard() {
+  const boards = useBoards();
+
   return (
     <div className="w-[15rem] lg:w-[17rem] h-full py-4  border-r border-r-zinc-600/60  dark:border-r-zinc-400/60">
       <div className="flex flex-col">
         <header className="font-bold text-lg px-3 text-zinc-800/80 dark:text-zinc-200/80">
           Boards
         </header>
-        <section className="my-3  pb-3 border-b border-b-zinc-600/60 dark:border-b-zinc-400/60">
+        <section className="my-3  pb-3  dark:border-b-zinc-400/60">
           <ul className="mx-6 space-y-2 ">
-            {[1, 2, 3, 4, 5].map((index) => {
+            {/* {[1, 2, 3, 4, 5].map((index) => {
               return (
                 <li className="text-sm font-semibold dark:text-zinc-400">
                   Project {index}
                 </li>
               );
-            })}
+            })} */}
+            {/* todo: Update Loading Component */}
+            {boards.loading ? (
+              <div>Loading Cuz</div>
+            ) : (
+              boards.boards.map((board) => {
+                return (
+                  <li
+                    key={board.boardID}
+                    className="text-sm font-semibold dark:text-zinc-400"
+                  >
+                    <Link to={`board/${board.boardID}`}>{board.boardName}</Link>
+                  </li>
+                );
+              })
+            )}
           </ul>
         </section>
       </div>
---
+++ Client/src/Hooks/useBoards.ts
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from "react";
+import { Auth, getAuth, onAuthStateChanged } from "firebase/auth";
+import axios from "axios";
+import { TDashTile } from "@/Types/TDashTile";
+
+function useBoards() {
+  const [boards, setBoards] = useState<TDashTile[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const fetchBoards = async () => {
+        const auth: Auth = getAuth();
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          console.log("User is signed in");
+          axios
+          .get(`http://localhost:3000/api/boards/${user.uid}`)
+          .then((res) => {
+            //Returns an array of board id
+            const boardIDs = res.data;
+            axios
+              .post(`http://localhost:3000/api/boards/userBoards`, {
+                boardIDs: boardIDs,
+                userID: user.uid,
+              })
+              .then((res) => {
+                setBoards(res.data);
+                setLoading(false);
+              })
+              .catch((err) => {
+                console.log(err);
+                setError(true);
+              });
+          })
+          .catch((err) => {
+            console.log(err);
+            setError(true);
+          });
+      
+        } else {
+          console.log("No user is signed in");
+        }
+      })
+
+      
+      
+    };
+    fetchBoards();
+  }, []);
+
+  return { boards, error, loading };
+}
+
+export default useBoards;

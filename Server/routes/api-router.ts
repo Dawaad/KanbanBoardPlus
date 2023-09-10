851ff919783942fb5edf9e3f3fed429fbf93d2ce
Liam O'Kane <loka0001@student.monash.edu>
2023-09-10 13:38:17 +1000

    added columns routing

--- Server/routes/api-router.ts
+++ Server/routes/api-router.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import tasksRouter from './tasks-router';
 import boardsRouter from './boards-router';
 import authRouter from './auth-router';
+import columnsRouter from "./columns-router";
 
 const apiRouter = Router();
 
 apiRouter.use(authRouter);
 apiRouter.use('/tasks', tasksRouter)
 apiRouter.use('/boards', boardsRouter);
+apiRouter.use('/columns', columnsRouter);
 
 export default apiRouter;

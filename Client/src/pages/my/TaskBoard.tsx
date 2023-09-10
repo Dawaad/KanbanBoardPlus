a7b6bd8f50df176f9763a6a211211001103fde9a
Dawaad <jntucker02@gmail.com>
2023-09-10 20:02:03 +1000

    Implemented Invite Functionality

--- Client/src/pages/my/TaskBoard.tsx
+++ Client/src/pages/my/TaskBoard.tsx
@@ -1,13 +1,18 @@
 import { useState, useEffect } from "react";
 import TaskBoardComp from "../../Components/Task/Board/Taskboard";
-import TaskSideBar from "../../Components/Task/Board/TaskSideBar";
-import { useParams } from "react-router-dom";
+import TaskSideBar from "../../Components/Task/Board/Sidebar/TaskSideBar";
+import { useNavigate, useParams } from "react-router-dom";
 import useBoard from "@/Hooks/useBoard";
+import LoadingSkeleton from "@/Components/Task/Board/LoadingSkeleton";
 function TaskBoard() {
   //Retrieve Board ID from params
   const { boardID } = useParams();
-  const board = useBoard(boardID);
-  console.log(board)
+  const navigate = useNavigate();
+  if (!boardID) {
+    navigate("/dashboard");
+  }
+  //Commented Out while I work on other features not including the board
+  // const board = useBoard(boardID);
 
   //Access Firebase With Board ID
 
@@ -17,11 +22,10 @@ function TaskBoard() {
     <main className="overflow-hidden flex flex-row h-[100dvh]">
       <div className="absolute w-full  h-[50rem] bg-gradient-to-br from-pink-400 to-blue-500 dark:from-purple-400 dark:to-red-600 rounded-md filter blur-3xl opacity-60 -z-10"></div>
 
-      <TaskSideBar />
+      <TaskSideBar boardID={boardID as string} />
 
       <section className="overflow-x-scroll my-2 scrollbar scrollbar-thumb-zinc-700/50 dark:scrollbar-thumb-zinc-400/70 scrollbar-thumb-rounded-lg flex ">
-        <TaskBoardComp />
-        <div>Hey</div>
+        {/* {board.loading ? <LoadingSkeleton/> : <TaskBoardComp />} */}
       </section>
     </main>
   );

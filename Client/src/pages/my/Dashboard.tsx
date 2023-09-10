29fdc57713ff07983008ae1a8a6526eee2a5eb62
Dawaad <jntucker02@gmail.com>
2023-09-10 20:01:47 +1000

    Update Board Retrieving Structure

--- Client/src/pages/my/Dashboard.tsx
+++ Client/src/pages/my/Dashboard.tsx
@@ -7,8 +7,9 @@ import useBoardTiles from "@/Hooks/useBoardsTile";
 import axios from "axios";
 import { Skeleton } from "@/Components/ui/skeleton";
 import DashboardTile from "../../Components/Dash/DashboardTile";
-import CreateModal from "../../Components/Dash/Create";
+import CreateModal from "../../Components/Dash/Modal/Create";
 import LoadingTiles from "@/Components/Dash/LoadingTiles";
+import JoinInvite from "@/Components/Dash/Modal/Join";
 function Dashboard() {
   const navigate = useNavigate();
   const [user, setUser] = useState<User | null>(null);
@@ -38,7 +39,6 @@ function Dashboard() {
           <Sideboard />
         </aside>
         <main className="p-4">
-          
           <h1 className="hidden md:block md:text-2xl lg:text-4xl font-bold  my-4 text-zinc-800/80 dark:text-zinc-200">
             Hello, {user?.displayName}
           </h1>
@@ -53,34 +53,35 @@ function Dashboard() {
               {boards.loading ? (
                 <LoadingTiles />
               ) : (
-                boards.boards
-                  .filter((board) => board.owner)
-                  .map((board) => {
-                    return (
-                      <DashboardTile
-                        boardDetails={{
-                          id: board.boardID,
-                          name: board.boardName,
-                        }}
-                      />
-                    );
-                  })
+                <>
+                  {boards.boards
+                    .filter((board) => board.owner)
+                    .map((board) => {
+                      return (
+                        <DashboardTile
+                          boardDetails={{
+                            id: board.boardID,
+                            name: board.boardName,
+                          }}
+                        />
+                      );
+                    })}
+                  <button
+                    onClick={() => {
+                      setCreateModal(true);
+                    }}
+                    className="m-4 md:w-[11rem] lg:w-[13rem] bg-zinc-300/70 hover:bg-zinc-400/70 dark:bg-zinc-700/30 dark:hover:bg-zinc-700/60 h-[7rem] transition-all  flex justify-center items-center shadow-md shadow-zinc-800 dark:shadow-none rounded-md"
+                  >
+                    <PlusIcon className="h-10 w-10 text-zinc-800/80 dark:text-zinc-200/80" />
+                  </button>
+                </>
               )}
-
-              <button
-                onClick={() => {
-                  setCreateModal(true);
-                }}
-                className="m-4 md:w-[11rem] lg:w-[13rem] bg-zinc-300/70 hover:bg-zinc-400/70 dark:bg-zinc-700/30 dark:hover:bg-zinc-700/60 h-[7rem] transition-all  flex justify-center items-center shadow-md shadow-zinc-800 dark:shadow-none rounded-md"
-              >
-                <PlusIcon className="h-10 w-10 text-zinc-800/80 dark:text-zinc-200/80" />
-              </button>
             </div>
           </section>
           <section id="shared-boards" className="m-4">
             <div className="flex flex-row items-center pb-4">
               <h1 className="text-lg md:text-2xl font-bold text-zinc-800/80 dark:text-zinc-200/80">
-                Shared Boards
+                Shared with you
               </h1>
             </div>
             <div className="grid grid-cols-2 md:flex flex-row flex-wrap p-4">
@@ -88,18 +89,21 @@ function Dashboard() {
               {boards.loading ? (
                 <LoadingTiles />
               ) : (
-                boards.boards
-                  .filter((board) => !board.owner)
-                  .map((board) => {
-                    return (
-                      <DashboardTile
-                        boardDetails={{
-                          id: board.boardID,
-                          name: board.boardName,
-                        }}
-                      />
-                    );
-                  })
+                <>
+                  {boards.boards
+                    .filter((board) => !board.owner)
+                    .map((board) => {
+                      return (
+                        <DashboardTile
+                          boardDetails={{
+                            id: board.boardID,
+                            name: board.boardName,
+                          }}
+                        />
+                      );
+                    })}
+                  <JoinInvite userID={user?.uid} />
+                </>
               )}
             </div>
           </section>
